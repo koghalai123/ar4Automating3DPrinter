f2ffe93796a3fb06d28e67a1ee2fbffbfe288f6b
Koghalai <koghalai@gmail.com>
2026-01-11 13:24:50 -0800

    added moving camera

--- launchVirtualRobot.sh
+++ launchVirtualRobot.sh
@@ -8,6 +8,4 @@ sleep 6
 
 sleep 2
 
-ros2 launch annin_ar4_moveit_config moveit.launch.py use_sim_time:=true include_gripper:=True &
-sleep 2
-ros2 run ros_gz_bridge parameter_bridge   /rgb_camera/image@sensor_msgs/msg/Image[gz.msgs.Image   /rgb_camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo
+ros2 launch annin_ar4_moveit_config moveit.launch.py use_sim_time:=true include_gripper:=True
